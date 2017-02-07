c1a314f5c9e0fbbfa77bc1db0238168d76028523
Iggy <igoririanto@rocketmail.com>
2017-02-06 18:41:06 -0800

    first commit hello react

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      txt: 'state text!'
+    }
+  }
+  update(e){
+    this.setState({
+      txt: e.target.value
+    })
+  }
+  render (){
+    return (
+      <div>
+        <input type="text" onChange={this.update.bind(this)} />
+        <h1>{this.state.txt}</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
